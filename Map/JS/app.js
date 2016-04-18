427b370d51398de983e420bb925887918f2a51a7
Johnson Lee <Johnson Lee>
2016-04-18 15:21:53 +0800

    更改map click變成hover

--- Map/JS/app.js
+++ Map/JS/app.js
@@ -1,97 +1,97 @@
 var main = function() {
 
-    $('.map_info').click(function() {
-        $('.active-map_info_pop_up').fadeOut(0).removeClass('active-map_info_pop_up');
-    });
-
-    $('#place1').click(function() {
+    $('#place1').hover(function() {
         $('#place1').find("div").fadeIn(200).addClass('active-map_info_pop_up');
-    });
-    $('#place2').click(function() {
+    },function() {
+        $('.active-map_info_pop_up').fadeOut(0).removeClass('active-map_info_pop_up');
+    } );
+
+
+    $('#place2').hover(function() {
         $('#place2').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place3').click(function() {
+    $('#place3').hover(function() {
         $('#place3').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place4').click(function() {
+    $('#place4').hover(function() {
         $('#place4').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place5').click(function() {
+    $('#place5').hover(function() {
         $('#place5').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place6').click(function() {
+    $('#place6').hover(function() {
         $('#place6').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place7').click(function() {
+    $('#place7').hover(function() {
         $('#place7').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place8').click(function() {
+    $('#place8').hover(function() {
         $('#place8').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place9').click(function() {
+    $('#place9').hover(function() {
         $('#place9').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place10').click(function() {
+    $('#place10').hover(function() {
         $('#place10').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place11').click(function() {
+    $('#place11').hover(function() {
         $('#place11').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place12').click(function() {
+    $('#place12').hover(function() {
         $('#place12').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place13').click(function() {
+    $('#place13').hover(function() {
         $('#place13').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place14').click(function() {
+    $('#place14').hover(function() {
         $('#place14').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place15').click(function() {
+    $('#place15').hover(function() {
         $('#place15').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place16').click(function() {
+    $('#place16').hover(function() {
         $('#place16').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place17').click(function() {
+    $('#place17').hover(function() {
         $('#place17').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place18').click(function() {
+    $('#place18').hover(function() {
         $('#place18').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place19').click(function() {
+    $('#place19').hover(function() {
         $('#place19').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place20').click(function() {
+    $('#place20').hover(function() {
         $('#place20').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place21').click(function() {
+    $('#place21').hover(function() {
         $('#place21').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place22').click(function() {
+    $('#place22').hover(function() {
         $('#place22').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place23').click(function() {
+    $('#place23').hover(function() {
         $('#place23').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place24').click(function() {
+    $('#place24').hover(function() {
         $('#place24').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place25').click(function() {
+    $('#place25').hover(function() {
         $('#place25').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place26').click(function() {
+    $('#place26').hover(function() {
         $('#place26').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place27').click(function() {
+    $('#place27').hover(function() {
         $('#place27').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place28').click(function() {
+    $('#place28').hover(function() {
         $('#place28').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place29').click(function() {
+    $('#place29').hover(function() {
         $('#place29').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
-    $('#place30').click(function() {
+    $('#place30').hover(function() {
         $('#place30').find("div").fadeIn(200).addClass('active-map_info_pop_up');
     });
 
